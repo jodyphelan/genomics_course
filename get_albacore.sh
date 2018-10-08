fe01bf2281062ec0a60be978fb535829b4002351
jodyphelan <jody.phelan@lshtm.ac.uk>
2018-10-08 15:15:24 +0100

    remove install albacore script

--- get_albacore.sh
+++
@@ -1,3 +0,0 @@
-wget https://mirror.oxfordnanoportal.com/software/analysis/ont_albacore-2.3.3-cp36-cp36m-manylinux1_x86_64.whl
-pip install ont_albacore-2.3.3-cp36-cp36m-manylinux1_x86_64.whl
-mv /var/www/html/Haploview.jar /usr/local/bin/
