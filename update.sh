83944bc5d5aad2d448c2ab625092aa42a86bdde5
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-10-07 10:09:35 +0100

    update

--- update.sh
+++ update.sh
@@ -4,3 +4,7 @@ git clone https://github.com/jodyphelan/genomics_course.git
 mamba env create --force -f genomics_course/conda_env/assembly.yaml
 mamba env create --force -f genomics_course/conda_env/nanopore.yaml
 
+eval "$(conda shell.bash hook)"
+conda activate nanopore
+pip install /usr/local/src/ont_alba*
+
