47df68d2133a7b4e3c719b1c0b02660fb1896cef
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-10-07 10:01:09 +0100

    add

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # Any updates
 cd /home/user/Documents
 git clone https://github.com/jodyphelan/genomics_course.git
-mamba env create --force -f genomics_course/conda_env/assebmly.yml
-mamba env create --force -f genomics_course/conda_env/nanopore.yml
+mamba env create --force -f genomics_course/conda_env/assembly.yaml
+mamba env create --force -f genomics_course/conda_env/nanopore.yaml
 
