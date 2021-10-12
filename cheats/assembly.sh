a2389b86431194e3df833ab7ff20505fd706cbc3
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-10-12 20:14:37 +0100

    asm

--- cheats/assembly.sh
+++ cheats/assembly.sh
@@ -5,6 +5,7 @@ zcat sample1_1.fastq.gz | head -8
 wc -l *
 zcat sample1_1.fastq | head -2 | tail -1 | wc -c
 
+cd ~/data/tb
 spades.py -1 ~/data/tb/sample1_1.fastq.gz -2  ~/data/tb/sample1_2.fastq.gz -o sample1_asm
 cd sample1_asm
 quast -r ../tb.fasta -o quast contigs.fasta 
@@ -15,6 +16,7 @@ bwa index -a is sample1_asm.fasta
 bwa mem -k 20 -c 100 -L 20 -U 20 -M -T 50 sample1_asm.fasta ~/data/tb/sample1_1.fastq.gz ~/data/tb/sample1_2.fastq.gz | samtools sort -o sample1_asm.bam
 samtools index sample1_asm.bam
 
+cd ~/data/tb
 spades.py -1 ~/data/tb/sample2_1.fastq.gz -2  ~/data/tb/sample2_2.fastq.gz -o sample2_asm
 cd sample2_asm
 quast -r ../tb.fasta -o quast contigs.fasta 
@@ -25,6 +27,7 @@ bwa index -a is sample2_asm.fasta
 bwa mem -k 20 -c 100 -L 20 -U 20 -M -T 50 sample2_asm.fasta ~/data/tb/sample2_1.fastq.gz ~/data/tb/sample2_2.fastq.gz | samtools sort -o sample2_asm.bam
 samtools index sample2_asm.bam
 
+cd ~/data/tb
 spades.py -1 ~/data/tb/sample3_1.fastq.gz -2  ~/data/tb/sample3_2.fastq.gz -o sample3_asm
 cd sample3_asm
 quast -r ../tb.fasta -o quast contigs.fasta 
