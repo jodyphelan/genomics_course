26c4a325b1f810871ff80f3cbfb96aad9b54976c
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2021-10-15 11:03:58 +0100

    add taxonomy to microbiome intermediate files

--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@ cd ~/data/metagenomics/
 wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/denoising_stats.qza
 wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/rep_seqs.qza
 wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/table.qza
+wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/taxonomy.qza
