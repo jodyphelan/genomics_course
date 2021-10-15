4049974380db6937eb62d6723f67f971a2a66021
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2021-10-15 11:08:09 +0100

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 # Any updates
 cd ~/data/metagenomics/
-wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/denoising_stats.qza
-wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/rep_seqs.qza
-wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/table.qza
-wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/taxonomy.qza
+wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/denoising_stats.qza \
+	-O denoising_stats.qza
+wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/rep_seqs.qza \
+	-O rep_seqs.qza
+wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/table.qza \
+	-O table.qza
+wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/taxonomy.qza \
+	-O taxonomy.qza
