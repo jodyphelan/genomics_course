020c296aa167e344c826af80488c3937f1c891c9
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2021-11-20 03:33:50 +0000

    update

--- scripts/setup_machine/04_get_data.sh
+++ scripts/setup_machine/04_get_data.sh
@@ -1,5 +1,6 @@
 pip install gdown
 cd
-gdown https://drive.google.com/uc?id=1nIy0DZl1p_nR-bAvYtfkiI_-B68Q7Xzy -O data.tar.gz
-tar -xzvf data.tar.gz
-rm data.tar.gz
+gdown https://drive.google.com/uc?id=1nIy0DZl1p_nR-bAvYtfkiI_-B68Q7Xzy
+tar -xzvf course_data.tar.gz
+rm course_data.tar.gz
+mv course_data data
