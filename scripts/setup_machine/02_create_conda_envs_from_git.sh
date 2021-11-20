b0040da9c1b73fc4cfbf3ccd91f040fd00d0bc4b
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2021-11-20 01:25:33 +0000

    update

--- scripts/setup_machine/02_create_conda_envs_from_git.sh
+++ scripts/setup_machine/02_create_conda_envs_from_git.sh
@@ -1,5 +1,4 @@
 eval "$(conda shell.bash hook)"
 conda install -n base -c conda-forge mamba -y
-for file in ~/git/genomics_course/conda_env/*yaml; do 
-	mamba env create -f $file
-done
+ls ~/git/genomics_course/conda_env/*yaml | xargs -n1 -P4 \
+	mamba env create -f
--- scripts/setup_machine/setup.sh
+++ scripts/setup_machine/setup.sh
@@ -1,6 +1,7 @@
+mkdir -p ~/.vim/{backup,swap,undo}
 mkdir ~/git
 cd ~/git
 git clone https://github.com/julibeg/dotfiles.git
 find ~/git/dotfiles/ -maxdepth 1 -type f -exec ln -s {} ~/ \;
 git clone https://github.com/jodyphelan/genomics_course.git
-#source ~/git/genomics_course/scripts/setup_machine/*sh
+source ~/git/genomics_course/scripts/setup_machine/0*sh
