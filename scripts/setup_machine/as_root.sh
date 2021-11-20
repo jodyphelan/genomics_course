44081084ca2727ae6c00b55a2946e0e721a18fe8
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2021-11-20 01:45:22 +0000

    update

---
+++ scripts/setup_machine/as_root.sh
@@ -0,0 +1,9 @@
+apt update
+apt install -y default-jre xclip
+useradd -m -s /bin/bash user
+echo user:8eyMByVWFjgy6Na | chpasswd
+sudo -i -u user bash << EOF
+wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/scripts/setup_machine/as_user.sh
+bash as_user.sh
+pwd
+EOF
