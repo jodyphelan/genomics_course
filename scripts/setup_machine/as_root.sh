84010c690571ff5a7abb0797facb4fbc2e327bfd
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2021-11-20 02:24:59 +0000

    update

--- scripts/setup_machine/as_root.sh
+++ scripts/setup_machine/as_root.sh
@@ -5,5 +5,4 @@ echo user:8eyMByVWFjgy6Na | chpasswd
 sudo -i -u user bash << EOF
 wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/scripts/setup_machine/as_user.sh
 bash as_user.sh
-pwd
 EOF
--- scripts/setup_machine/as_user.sh
+++ scripts/setup_machine/as_user.sh
@@ -4,4 +4,4 @@ cd ~/git
 git clone https://github.com/julibeg/dotfiles.git
 find ~/git/dotfiles/ -maxdepth 1 -type f -exec ln -s {} ~/ \;
 git clone https://github.com/jodyphelan/genomics_course.git
-source <(cat ~/git/genomics_course/scripts/setup_machine/0*sh)
+source <(cat ~/git/genomics_course/scripts/setup_machine/01*sh)
