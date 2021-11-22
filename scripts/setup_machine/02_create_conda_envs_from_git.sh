ea794675c12964444b1375bb1fb9581f5a461492
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2021-11-22 03:12:07 +0000

    update

--- scripts/setup_machine/02_create_conda_envs_from_git.sh
+++ scripts/setup_machine/02_create_conda_envs_from_git.sh
@@ -1,4 +1,4 @@
 eval "$(conda shell.bash hook)"
 conda install -n base -c conda-forge mamba -y
-ls ~/git/genomics_course/conda_env/*yaml | xargs -n1 -P4 \
-	mamba env create -f
+ls ~/git/genomics_course/conda_env/*yaml | xargs -n1 -i -P4 \
+	mamba env create -f {} --force
