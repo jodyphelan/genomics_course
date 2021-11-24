4b93e8118acc6b9542d8764da8ec50d1cba1144c
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2021-11-24 11:46:20 +0000

    link update_course.sh script to ~/bin on remote machine setup

--- scripts/setup_machine/as_user.sh
+++ scripts/setup_machine/as_user.sh
@@ -5,3 +5,5 @@ git clone https://github.com/julibeg/dotfiles.git
 find ~/git/dotfiles/ -maxdepth 1 -type f -exec ln -s {} ~/ \;
 git clone https://github.com/jodyphelan/genomics_course.git
 source <(cat ~/git/genomics_course/scripts/setup_machine/0*sh)
+ln -s ~/git/genomics_course/scripts/update_course.sh ~/bin
+chmod +x ~/bin/update_course.sh
