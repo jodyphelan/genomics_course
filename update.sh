aa756ee1d8ac0c3a1ba0c50ef19a1f3069cb5a30
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-11-24 11:19:51 +0000

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 # Any updates
-cd ~/data/metagenomics/
-wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/denoising_stats.qza
-wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/rep_seqs.qza
-wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/table.qza
-wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/intermediate_files/taxonomy.qza
-rm *.1
+conda activate base
+wget https://raw.githubusercontent.com/jodyphelan/genomics_course/master/conda_env/nanopore.yaml
+mamba env create -f nanopore.yaml -y
+rm nanopore.yaml
