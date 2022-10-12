1d9c70077b683fb2bcb8c1f300636566722d3c08
dan-ward-bio <42579972+dan-ward-bio@users.noreply.github.com>
2022-10-12 09:08:15 +0700

    update script update

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 # Any updates
-cd ~/data/tb
-mkdir bedaquiline
-cd bedaquiline
-wget https://tbdr.lshtm.ac.uk/static/files.tgz
-tar -xvf files.tgz
-rm files.tgz
+#cd ~/data/tb
+#mkdir bedaquiline
+#cd bedaquiline
+#wget https://tbdr.lshtm.ac.uk/static/files.tgz
+#tar -xvf files.tgz
+#rm files.tgz
 cd ~/Documents
 rm -r ~/Documents/genomics_course/
 git clone https://github.com/jodyphelan/genomics_course.git
