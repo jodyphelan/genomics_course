07435fe9913c639021f3b0a4b55905dd32f86a81
dan-ward-bio <42579972+dan-ward-bio@users.noreply.github.com>
2022-10-12 09:06:50 +0700

    update script update

--- update.sh
+++ update.sh
@@ -6,5 +6,6 @@ wget https://tbdr.lshtm.ac.uk/static/files.tgz
 tar -xvf files.tgz
 rm files.tgz
 cd ~/Documents
+rm -r ~/Documents/genomics_course/
 git clone https://github.com/jodyphelan/genomics_course.git
 cp ~/Documents/genomics_course/cheats/temp.krak ~/data/nanopore_activity/kraken/.temp.krak
