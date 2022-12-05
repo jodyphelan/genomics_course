abc2245d28e88430fcdd8e5532a95cb5fac2b8f0
dan-ward-bio <42579972+dan-ward-bio@users.noreply.github.com>
2022-12-05 22:00:41 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -24,5 +24,4 @@ export PLATFORM=$(lsb_release -cs)
 wget -O- https://cdn.oxfordnanoportal.com/apt/ont-repo.pub | sudo apt-key add -
 echo "deb http://cdn.oxfordnanoportal.com/apt ${PLATFORM}-stable non-free" | sudo tee /etc/apt/sources.list.d/nanoporetech.sources.list
 sudo apt update
-sudo apt update
 sudo apt install ont-guppy-cpu
