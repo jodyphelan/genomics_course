d2fa72229b83e024abd5228dc16348e111f47e27
Jody Phelan <jody.phelan@lshtm.ac.uk>
2023-09-08 19:19:23 +0100

    update env creation

--- scripts/setup_machine/02_create_conda_envs_from_git.sh
+++ scripts/setup_machine/02_create_conda_envs_from_git.sh
@@ -1,12 +1,13 @@
 eval "$(conda shell.bash hook)"
 conda install -n base -c conda-forge mamba -y
-conda create --name assembly --file ~/git/genomics_course/conda_env/assembly.explicit.txt
-conda create --name eqtl --file ~/git/genomics_course/conda_env/eqtl.explicit.txt
-conda create --name gwas --file ~/git/genomics_course/conda_env/gwas.explicit.txt
-conda create --name mapping --file ~/git/genomics_course/conda_env/mapping.explicit.txt
-conda create --name methylation --file ~/git/genomics_course/conda_env/methylation.explicit.txt
-conda create --name microbiome --file ~/git/genomics_course/conda_env/microbiome.explicit.txt
-conda create --name nanopore --file ~/git/genomics_course/conda_env/nanopore.explicit.txt
-conda create --name phylogenetics --file ~/git/genomics_course/conda_env/phylogenetics.explicit.txt
-conda create --name rnaseq --file ~/git/genomics_course/conda_env/rnaseq.explicit.txt
-conda create --name variant_detection --file ~/git/genomics_course/conda_env/variant_detection.explicit.txt
+mkdir ~/logs
+conda create --name assembly --file ~/git/genomics_course/conda_env/assembly.explicit.txt 2> ~/logs/assembly.log
+conda create --name eqtl --file ~/git/genomics_course/conda_env/eqtl.explicit.txt 2> ~/logs/eqtl.log
+conda create --name gwas --file ~/git/genomics_course/conda_env/gwas.explicit.txt 2> ~/logs/gwas.log
+conda create --name mapping --file ~/git/genomics_course/conda_env/mapping.explicit.txt 2> ~/logs/mapping.log
+conda create --name methylation --file ~/git/genomics_course/conda_env/methylation.explicit.txt 2> ~/logs/methylation.log
+conda create --name microbiome --file ~/git/genomics_course/conda_env/microbiome.explicit.txt 2> ~/logs/microbiome.log
+conda create --name nanopore --file ~/git/genomics_course/conda_env/nanopore.explicit.txt 2> ~/logs/nanopore.log
+conda create --name phylogenetics --file ~/git/genomics_course/conda_env/phylogenetics.explicit.txt 2> ~/logs/phylogenetics.log
+conda create --name rnaseq --file ~/git/genomics_course/conda_env/rnaseq.explicit.txt 2> ~/logs/rnaseq.log
+conda create --name variant_detection --file ~/git/genomics_course/conda_env/variant_detection.explicit.txt 2> ~/logs/variant_detection.log
